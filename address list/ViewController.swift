9c63d23cf69494f839ce0368721742c3978f8a9c
wyqzl <wuyuqian@admindeimac-35.local>
2016-07-06 09:29:41 +0800

    Initial Commit

---
+++ address list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  address list
+//
+//  Created by 吴玉倩 on 16/7/6.
+//  Copyright © 2016年 吴玉倩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
